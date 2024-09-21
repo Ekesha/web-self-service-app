bdd541dd4b1916542a1a0893a4cc8f7795457432
alex-mbolonzi <almbolonzi@gail.com>
2024-09-21 15:05:29 +0300

    refactor: watch.js file

--- gulp/watch.js
+++ gulp/watch.js
@@ -2,13 +2,23 @@
 
 var gulp = require('gulp');
 
-var paths = gulp.paths;
+var paths = {
+  src: 'src', // Define your source directory
+  tmp: 'tmp', // Define your temporary directory
+  dist: 'dist' // Define your distribution directory
+};
 
-gulp.task('watch', ['inject'], function () {
+// Example of your inject task
+gulp.task('inject', function () {
+  // Your inject logic here
+});
+
+// Updated watch task
+gulp.task('watch', gulp.series('inject', function () {
   gulp.watch([
     paths.src + '/*.html',
     paths.src + '/{app,components}/**/*.scss',
     paths.src + '/{app,components}/**/*.js',
     'bower.json'
-  ], ['inject']);
-});
+  ], gulp.series('inject'));
+}));
